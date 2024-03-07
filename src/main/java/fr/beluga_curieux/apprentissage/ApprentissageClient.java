4bb8d20ada6cbd80af964920f204db5938fa5a7c
beluga curieux <beluga.curieux@gmail.com>
2024-03-07 22:24:19 +0100

    make som seting

---
+++ src/main/java/fr/beluga_curieux/apprentissage/ApprentissageClient.java
@@ -0,0 +1,10 @@
+package fr.beluga_curieux.apprentissage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ApprentissageClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
