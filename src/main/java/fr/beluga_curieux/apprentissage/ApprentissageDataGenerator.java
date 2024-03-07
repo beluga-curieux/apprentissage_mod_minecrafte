24933a035b153e963c9500fd31f96ed9bfc9b2f4
beluga curieux <beluga.curieux@gmail.com>
2024-03-07 22:08:19 +0100

    Initial commit

---
+++ src/main/java/fr/beluga_curieux/apprentissage/ApprentissageDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.beluga_curieux.apprentissage;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ApprentissageDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
